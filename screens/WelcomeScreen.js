8b502a68e244418fd97b777e2705bc58225f3198
ragg09 <ragunayon@gmail.com>
2023-08-16 17:06:19 +0800

    Accessinng protected resource using token

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchMessage, setFetchMessage] = useState();
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://react-native-course-43a66-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=' +
+          authCtx.token
+      )
+      .then((response) => {
+        setFetchMessage(response.data);
+      });
+  }, [authCtx.token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
-      <Text>You authenticated successfully!</Text>
+      <Text>{fetchMessage}</Text>
     </View>
   );
 }
@@ -16,11 +33,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 32,
+    padding: 32
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 8,
-  },
+    marginBottom: 8
+  }
 });
