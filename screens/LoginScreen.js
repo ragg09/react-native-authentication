fa99e5ffa808127670252ab96e14e0524972bfd0
ragg09 <ragunayon@gmail.com>
2023-08-16 15:14:24 +0800

    Add Login Functionality

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { login } from '../util/auth';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const signInHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message={'Logging User . . .'} />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={signInHandler} />;
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,13 +1,22 @@
 import axios from 'axios';
 import { FIREBASE_API_KEY } from '@env';
 
+const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${FIREBASE_API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true
+  });
+
+  console.log(response.data);
+};
+
 export const createUser = async (email, password) => {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }
-  );
+  await authenticate('signUp', email, password);
+};
+
+export const login = async (email, password) => {
+  await authenticate('signInWithPassword', email, password);
 };
